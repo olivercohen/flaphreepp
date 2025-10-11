2d80ea85fe2f8e95d42536dc1c2acfb8a9a900ce
Oliver <oliver.stjernberg@gmail.com>
2025-10-12 01:14:55 +0200

    brought together all constants in a single header.

--- constants.hpp
+++ constants.hpp
@@ -4,7 +4,20 @@
 
 #pragma once
 
-namespace constants {
-    inline constexpr float GRAVITY = -20.0f;
-    inline constexpr float PIPE_SPEED = -10.0f;
+namespace birdcfg {
+    constexpr float GRAVITY = -20.0f;
+    constexpr float FLAP_VEL = 10.0f;
+}
+
+namespace pipecfg {
+    constexpr int NUM_PIPES         = 6;
+    constexpr float PIPE_SPEED      =-10.0f;   // negativ for å bevege seg mot venstre
+    constexpr float PIPE_RADIUS     = 1.0f;
+    constexpr float PIPE_HEIGHT     = 20.0f;
+    constexpr float SPACING         = 20.0f;    // avstand mellom pipe-par
+    constexpr float RESPAWN_X       = 30.0f;    // hvor pipes respawner til høyre
+    constexpr float DESPAWN_X       = RESPAWN_X - NUM_PIPES*SPACING;   // når pipes er forbi kamera til venstre
+    constexpr float GAP_SIZE        = 4.0f;     // vertikal gap mellom topp og bunn
+    constexpr float RAND_MIN_Y      =-7.0f;   // random range for senter-Y
+    constexpr float RAND_MAX_Y      = 8.0f;
 }
